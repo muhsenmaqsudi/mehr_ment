5fd8c2d8169d20507baff8421e798bc6167cc93c
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2019-12-27 22:13:16 +0330

    changed testing environment to test for suiting jest test env

--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -3,7 +3,7 @@ import ts from "typescript";
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: "development" | "testing" | "production";
+      NODE_ENV: "development" | "test" | "production";
       PORT?: string;
       NODE_CONFIG_DIR: string;
       MONGODB_URI: string;

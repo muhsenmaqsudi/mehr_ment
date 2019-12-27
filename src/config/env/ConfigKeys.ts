c0c641bed38b9288b87ab4d307d0ffac16509ec0
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2019-12-27 17:00:27 +0330

    added database configs settings, and MONGODB_URL env variable

--- src/config/env/ConfigKeys.ts
+++ src/config/env/ConfigKeys.ts
@@ -1,7 +1,12 @@
 export enum ConfigKeys {
-  SERVER = "server"
+  SERVER = "server",
+  DATABASE = "database"
 }
 
 export interface ServerConfig {
   port: string;
+}
+
+export interface DatabaseConfig {
+  URI?: string;
 }
--- src/typings/environment.d.ts
+++ src/typings/environment.d.ts
@@ -3,9 +3,10 @@ import ts from "typescript";
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: "development" | "production";
+      NODE_ENV: "development" | "testing" | "production";
       PORT?: string;
       NODE_CONFIG_DIR: string;
+      MONGODB_URI: string;
     }
   }
 }
